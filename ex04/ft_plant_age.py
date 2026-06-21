b123c30054f8947d3bf3733a1b17f1fc272cdefc
oussama7007 <oussamatachakourt@gmail.com>
2026-06-21 02:23:43 +0100

    finish ex04

---
+++ ex04/ft_plant_age.py
@@ -0,0 +1,13 @@
+
+def ft_plant_age():
+
+        age = int(input("Enter plant age in days: "))
+
+        if age > 60:
+            print("Plant is fully grown.")
+        else:
+            print("Plant needs more time to grow.")
+
+ 
+
+ft_plant_age()
